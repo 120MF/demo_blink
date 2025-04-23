9f4311864f83e6bd12a9034f1ccb3071ba866277
MoonFeather <moonbite233@gmail.com>
2025-04-23 21:50:49 +0800

    add log, fix pwm

--- src/main.c
+++ src/main.c
@@ -4,30 +4,31 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/drivers/led.h>
+#include <zephyr/logging/log.h>
 
-
-#define UART_DEVICE_NODE DT_NODELABEL(usart1)
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 int main(void) {
 	int ret = 0;
 
-	const struct device *led_dev = DEVICE_DT_GET(DT_NODELABEL(pwmleds));
+	const struct device *led_dev = DEVICE_DT_GET(DT_NODELABEL(leds));
 	if (!device_is_ready(led_dev)) {
-		printk("LED设备未就绪\n");
+		LOG_ERR("LED设备未就绪\n");
 		return 0;
 	}
 	ret = led_set_brightness(led_dev, 0, 30) ||
 	      led_set_brightness(led_dev, 1, 20) ||
 	      led_set_brightness(led_dev, 2, 50);
 	if (ret) {
-		printk("设置亮度失败: %d\n", ret);
+		LOG_ERR("设置亮度失败: %d\n", ret);
 	}
 
 	while (1) {
-		printk("Program running... \n");
+		LOG_INF("程序运行中... \n");
+		k_sleep(K_SECONDS(1U));
 
-		// Sleep
-		k_sleep(K_SECONDS(4U));
+		printk("测试Console\n");
+		k_sleep(K_SECONDS(1U));
 	}
 
 	return 0;
